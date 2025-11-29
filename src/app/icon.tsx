e328118e7d7da13b6c4fe16fcdd661ef363f399e
yogeshghotia857-jpg <yogeshghotia857@gmail.com>
2025-11-29 23:00:44 +0530

    Update logo with new SVG paths

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -27,12 +27,16 @@ export default function Icon() {
                     xmlns="http://www.w3.org/2000/svg"
                     width="28"
                     height="14"
-                    viewBox="0 0 256 128"
+                    viewBox="0 0 135 75"
                     fill="none"
                 >
                     <path
                         fill="white"
-                        d="M96 128H32V96h64v32ZM224 32h-64v64h64v32h-96V0h96v32ZM32 96H0V32h32v64ZM256 96h-32V32h32v64ZM96 32H32V0h64v32Z"
+                        d="M90 15V0H135V15H90ZM75 30V15H90V30H75ZM90 30H120V45H90V30ZM135 60H120V45H135V60ZM120 60V75H75V60H120Z"
+                    />
+                    <path
+                        fill="white"
+                        d="M15 15V0H60V15H15ZM0 30V15H15V30H0ZM15 30H45V45H15V30ZM60 60H45V45H60V60ZM45 60V75H0V60H45Z"
                     />
                 </svg>
             </div>
--- src/components/chanhdai-mark.tsx
+++ src/components/chanhdai-mark.tsx
@@ -5,18 +5,22 @@ export function ChanhDaiMark(props: React.ComponentProps<"svg">) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
-      viewBox="0 0 256 128"
+      viewBox="0 0 135 75"
       {...props}
     >
       <path
         fill="currentColor"
-        d="M96 128H32V96h64v32ZM224 32h-64v64h64v32h-96V0h96v32ZM32 96H0V32h32v64ZM256 96h-32V32h32v64ZM96 32H32V0h64v32Z"
+        d="M90 15V0H135V15H90ZM75 30V15H90V30H75ZM90 30H120V45H90V30ZM135 60H120V45H135V60ZM120 60V75H75V60H120Z"
+      />
+      <path
+        fill="currentColor"
+        d="M15 15V0H60V15H15ZM0 30V15H15V30H0ZM15 30H45V45H15V30ZM60 60H45V45H60V60ZM45 60V75H0V60H45Z"
       />
     </svg>
   );
 }
 
 export function getMarkSVG(color: string) {
-  return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 256 128"><path fill="${color}" d="M96 128H32V96h64v32ZM224 32h-64v64h64v32h-96V0h96v32ZM32 96H0V32h32v64ZM256 96h-32V32h32v64ZM96 32H32V0h64v32Z"/></svg>`;
+  return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 135 75"><path fill="${color}" d="M90 15V0H135V15H90ZM75 30V15H90V30H75ZM90 30H120V45H90V30ZM135 60H120V45H135V60ZM120 60V75H75V60H120Z"/><path fill="${color}" d="M15 15V0H60V15H15ZM0 30V15H15V30H0ZM15 30H45V45H15V30ZM60 60H45V45H60V60ZM45 60V75H0V60H45Z"/></svg>`;
 }
 
